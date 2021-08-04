438465b71f996c9c51866e99aa704e1c6f38b4b0
LenouarMiloud <lenouarmiloud@gmail.com>
2021-08-04 19:26:44 +0200

    add the ItemAdapter and list_item

--- 3- AffirmationApp/app/src/main/java/com/fsociety/affirmationsapp/adapter/ItemAdapter.kt
+++ 3- AffirmationApp/app/src/main/java/com/fsociety/affirmationsapp/adapter/ItemAdapter.kt
@@ -1,4 +1,22 @@
 package com.fsociety.affirmationsapp.adapter
 
-class ItemAdapter {
+import com.fsociety.affirmationsapp.model.Affirmation
+
+class ItemAdapter(private val context: Context,private val dataset: List<Affirmation>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+    	TODO("Not yet implemented")
+    	val adapterLayout = LayoutInflater.from(parent.context)
+    	.inflate(R.layout.list_item, parent, false)
+	}
+
+override fun getItemCount() = dataset.size
+
+override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+	    TODO("Not yet implemented")
+	}
 }
+
