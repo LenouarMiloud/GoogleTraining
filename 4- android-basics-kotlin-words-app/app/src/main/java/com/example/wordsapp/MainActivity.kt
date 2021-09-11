a6f7d5ff1709c37353de6ba5fa17ecd565024344
LenouarMiloud <lenouarmiloud@gmail.com>
2021-09-11 13:38:48 +0200

    modify the words-app code

--- 4- android-basics-kotlin-words-app/app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ 4- android-basics-kotlin-words-app/app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -16,82 +16,42 @@
 package com.example.wordsapp
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.wordsapp.databinding.FragmentLetterListBinding
+import com.example.wordsapp.databinding.ActivityMainBinding
 
 /**
- * Entry fragment for the app. Displays a [RecyclerView] of letters.
+ * Main Activity and entry point for the app. Displays a RecyclerView of letters.
  */
-class LetterListFragment : Fragment() {
-    private var _binding: FragmentLetterListBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
+class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     // Keeps track of which LayoutManager is in use for the [RecyclerView]
     private var isLinearLayoutManager = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Retrieve and inflate the layout for this fragment
-        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
-    }
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         recyclerView = binding.recyclerView
-        // Sets the LayoutManager of the recyclerview
-        // On the first run of the app, it will be LinearLayoutManager
+        // Sets the LinearLayoutManager of the recyclerview
         chooseLayout()
     }
 
     /**
-     * Frees the binding object when the Fragment is destroyed.
-     */
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.layout_menu, menu)
-
-        val layoutButton = menu.findItem(R.id.action_switch_layout)
-        setIcon(layoutButton)
-    }
-
-    /**
      * Sets the LayoutManager for the [RecyclerView] based on the desired orientation of the list.
-     *
-     * Notice that because the enclosing class has changed from an Activity to a Fragment,
-     * the signature of the LayoutManagers has to slightly change.
      */
     private fun chooseLayout() {
         if (isLinearLayoutManager) {
-            recyclerView.layoutManager = LinearLayoutManager(context)
+            recyclerView.layoutManager = LinearLayoutManager(this)
         } else {
-            recyclerView.layoutManager = GridLayoutManager(context, 4)
+            recyclerView.layoutManager = GridLayoutManager(this, 4)
         }
         recyclerView.adapter = LetterAdapter()
     }
@@ -100,10 +60,30 @@ class LetterListFragment : Fragment() {
         if (menuItem == null)
             return
 
+        // Set the drawable for the menu icon based on which LayoutManager is currently in use
+
+        // An if-clause can be used on the right side of an assignment if all paths return a value.
+        // The following code is equivalent to
+        // if (isLinearLayoutManager)
+        //     menu.icon = ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+        // else menu.icon = ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
         menuItem.icon =
             if (isLinearLayoutManager)
-                ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_grid_layout)
-            else ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_linear_layout)
+                ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+    }
+
+    /**
+     * Initializes the [Menu] to be used with the current [Activity]
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_menu, menu)
+
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        // Calls code to set the icon based on the LinearLayoutManager of the RecyclerView
+        setIcon(layoutButton)
+
+        return true
     }
 
     /**
@@ -120,11 +100,11 @@ class LetterListFragment : Fragment() {
 
                 return true
             }
-            // Otherwise, do nothing and use the core event handling
+            //  Otherwise, do nothing and use the core event handling
 
             // when clauses require that all possible paths be accounted for explicitly,
-            // for instance both the true and false cases if the value is a Boolean,
-            // or an else to catch all unhandled cases.
+            //  for instance both the true and false cases if the value is a Boolean,
+            //  or an else to catch all unhandled cases.
             else -> super.onOptionsItemSelected(item)
         }
     }
