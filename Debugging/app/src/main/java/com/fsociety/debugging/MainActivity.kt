863f114615f0368ff7ad34cf9544c1f0e019e505
LenouarMiloud <lenouarmiloud@gmail.com>
2022-05-26 02:10:23 -0700

    add the debugging exemple

---
+++ Debugging/app/src/main/java/com/fsociety/debugging/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.fsociety.debugging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+        logging()
+    }
+
+    fun logging() {
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
+    }
+
+}
